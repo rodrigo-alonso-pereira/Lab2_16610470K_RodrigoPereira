c1fda1450e6d5b621e2c962030d3ae5e1c3ac95a
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-05-18 22:45:28 -0400

    Implementacion Constructor station de TDA station

---
+++ main_16610470_PereiraYañez.pl
@@ -0,0 +1,6 @@
+% Req 2: TDA station - constructor
+% 
+% Descripcion = Predicado constructor de una estación de metro
+% MP: station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]).
+station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]).
+
