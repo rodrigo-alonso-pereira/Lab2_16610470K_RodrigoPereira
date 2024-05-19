cfac3921ec5a13aca5e2c61fdcf5101302f67b18
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-05-18 23:21:08 -0400

    Implementacion Constructor section de TDA section

--- main_16610470_PereiraYañez.pl
+++ main_16610470_PereiraYañez.pl
@@ -1,6 +1,17 @@
-% Req 2: TDA station - constructor
-% 
-% Descripcion = Predicado constructor de una estación de metro
-% MP: station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]).
+/*
+Req 2: TDA station - constructor
+
+- Descripcion = Predicado constructor de una estación de metro.
+- MP: station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]).
+*/
 station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]).
+
+
+/*
+Req 3: TDA section - constructor.
+ 
+- Descripcion = Predicado que permite establecer enlaces entre dos estaciones.
+- MP: station(Point1, Point2, Distance, Cost, Section, [Point1, Point2, Distance, Cost, Section]).
+*/
+section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]).
 
