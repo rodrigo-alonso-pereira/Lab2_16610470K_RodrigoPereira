345c0e2689f0ee5e54689001cf7cd430c29dc18b
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-05-23 22:33:57 -0400

    Cambio en comentarios de MP

--- main_16610470_PereiraYañez.pl
+++ main_16610470_PereiraYañez.pl
@@ -2,7 +2,7 @@
 Req 2: TDA station - constructor
 
 - Descripcion = Predicado constructor de una estación de metro.
-- MP: station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]).
+- MP: station/5.
 */
 station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]).
 
@@ -11,7 +11,7 @@ station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]).
 Req 3: TDA section - constructor.
  
 - Descripcion = Predicado que permite establecer enlaces entre dos estaciones.
-- MP: station(Point1, Point2, Distance, Cost, Section, [Point1, Point2, Distance, Cost, Section]).
+- MP: section/5.
 */
 section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]).
 
@@ -20,7 +20,7 @@ section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]).
 Req 4: TDA line - constructor.
  
 - Descripcion = Predicado que permite crear una línea .
-- MP: station(Point1, Point2, Distance, Cost, Section, [Point1, Point2, Distance, Cost, Section]).
+- MP: line/5.
 */
 line(Id, Name, RailType, Sections, [Id, Name, RailType, Sections]).
 
