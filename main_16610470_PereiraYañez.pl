9c9b1d4f12971ca35b4878c785771ca5f2c07aa7
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-05-28 19:30:24 -0400

    Avances en lineLength TDA line

--- main_16610470_PereiraYañez.pl
+++ main_16610470_PereiraYañez.pl
@@ -19,8 +19,39 @@ section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]).
 /*
 Req 4: TDA line - constructor.
  
-- Descripcion = Predicado que permite crear una línea .
+- Descripcion = Predicado que permite crear una línea.
 - MP: line/5.
 */
 line(Id, Name, RailType, Sections, [Id, Name, RailType, Sections]).
+
+
+/*
+Req 5: TDA line - Otros predicados.
+ 
+- Descripcion = Predicado que permite determinar el largo total de una línea (cantidad de estaciones), 
+                la distancia (en la unidad de medida expresada en cada tramo) y su costo.
+- MP: line/5.
+- MS:
+*/
+
+pertenece(Elemento, [Elemento|_]].
+
+pertenece(Elemento, [_|Resto]) :-
+    pertenece(Elemento, Resto).
+
+distancia([], 0).
+
+distancia([_|Resto], Distancia) :-
+    distancia(Resto, Acc),
+    Distancia is Acc + 
+
+lineLength(L1, LENGTH) :-
+    line(_, _, _, Sections, L1),
+    section 
+    
+    
+    
+    
+
+
 
