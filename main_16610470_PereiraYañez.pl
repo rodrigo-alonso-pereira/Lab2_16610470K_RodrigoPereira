374576cf72605cc7e8f8105c0c6255ef89ec0351
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-05-28 23:30:16 -0400

    Mejora en comentarios

--- main_16610470_PereiraYañez.pl
+++ main_16610470_PereiraYañez.pl
@@ -1,57 +1,89 @@
 /*
-Script de Pruebas N°1
-Script actualizado al 11/04/2024 - 01:28 AM
+Req 2: TDA station - constructor
 
-station(1, "Los Heroes", t, 50, E1),
-station(2, "La moneda", r, 30, E2),
-station(3, "Universidad de Chile", t, 30, E3),
-line(1, "L1", “UIC 60 ASCE”, [], L1),
-section(E1, E2, 500, 100, S0),
-section(E2, E3, 550, 100, S1),
-lineAddSection(L1, S0, L1_1),
-lineAddSection(L1_1, S1, L1_2),
-subway(1, "Metro Santiago", Sub0),
-isLine(L1_2),
-% isLine(L1_1), % False porque falta estación terminal
-% subwayAddLine(Sub0, [L1_1], Sub2). % False porque la línea es inconsistente
-subwayAddLine(Sub0, [L1_2], Sub2).
-
+- Descripcion = Predicado constructor de una estación de metro.
+- MP: station/5.
 */
+station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]).
+
 
 /*
-Script de Prueba - Ejemplos
+Req 3: TDA section - constructor.
+ 
+- Descripcion = Predicado que permite establecer enlaces entre dos estaciones.
+- MP: section/5.
+*/
+section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]).
 
-% creando una nueva estación
-station( 0, "San Alberto Hurtado", "t", 35, ST0),
-station( 1, "USACH", "c", 30, ST1),
-station( 2, "Estación Central", "c", 45, ST2),
-station( 3, "ULA", "r", 45, ST3),
-station( 4, "República", "r", 45, ST4),
-station( 5, "Los Héroes", "c", 60, ST5),
-station( 6, "Toesca", "r", 40, ST6),
-station( 7, "La Moneda", "r", 40, ST7),
-station( 8, "Cochera", "m", 3600, ST8),
-station( 9, "Parque OHiggins", "r", 30, ST9),
-station( 10, "San Pablo", "t", 40, ST10),
-station( 11, "Los Dominicos", "t", 60, ST11),
+% Obtiene distancia de una seccion
+section_get_distance(Section, Distance) :-
+    section(_, _, Distance, _, Section).
 
-% creando una nueva sección
-section( ST0, ST1, 2, 50, S0),
-section( ST1, ST2, 2.5, 55, S1),
-section( ST2, ST3, 1.5, 30, S2),
-section( ST3, ST4, 3, 45, S3),
-section( ST4, ST5, 3, 45, S4),
-section( ST4, ST6, 1.4, 50, S5),
-section( ST5, ST8, 2,  40, S6_C),
-section( ST0, ST10, 3,  200, S7),
-section( ST7, ST11, 7, 200, S8),
-
-% creando lineas
-line( 0, "Línea 0", "UIC 60 ASCE", [ ], L0),
-line( 1, "Línea 1", "100 R.E.", [S0, S1, S2, S3, S5, S6_C, S7, S8], L1),
-
-% calculando largo, distancia y costo
-lineLength(L1, LENGTH, DISTANCE, COST).
+% Obtiene costo de una seccion
+section_get_cost(Section, Cost) :-
+    section(_, _, _, Cost, Section).
 
 
-*/ 
+/*
+Req 4: TDA line - constructor.
+ 
+- Descripcion = Predicado que permite crear una línea.
+- MP: line/5.
+*/
+line(Id, Name, RailType, Sections, [Id, Name, RailType, Sections]).
+
+% Obtiene lista de secciones de una linea
+line_get_sections(Line, Sections) :-
+    line(_, _, _, Sections, Line).
+
+
+/*
+Req 5: TDA line - Otros predicados.
+ 
+- Descripcion = Predicado que permite determinar el largo total de una línea (cantidad de estaciones), 
+                la distancia (en la unidad de medida expresada en cada tramo) y su costo.
+- MP: lineLength/4.
+- MS:
+*/
+
+% Imprimir elementos lista
+print_element(Element) :-
+    write(Element), 
+    nl.
+
+% Evalua si existe elemento en lista
+belongs(Element, [Element|_]).
+
+belongs(Element, [_|Tail]) :-
+    belongs(Element, Tail).
+
+% Recorre lista y obtiene elemento
+get_element_list([], _).
+
+get_element_list([First|Tail], Predicate) :-
+    call(Predicate, First),
+    get_element_list(Tail, Predicate).
+
+% Suma los elementos de una lista de TDAs
+sum_element_list([], 0, _).
+
+sum_element_list([Head|Tail], TotalSum, Predicate) :-
+    call(Predicate, Head, Element),
+    sum_element_list(Tail, Acc, Predicate),
+    TotalSum is Acc + Element.
+
+
+% 1 obtener lista de secciones. OK
+% 2 obtener cada seccion.
+% 3 obtener distancia y sumar en una acumulador.
+% 4 obtener costo y sumar en un acumulador.
+% 5 obtener estaciones y agregar en nueva lista, sin repetidos.
+% 6 obtener largo de nueva lista
+
+%lineLength(L1, LENGTH, DISTANCE, COST) :-
+lineLength(L1, DISTANCE, COST) :-
+    line_get_sections(L1, ListSections),
+    sum_element_list(ListSections, DISTANCE, section_get_distance),
+	sum_element_list(ListSections, COST, section_get_cost).
+    
+
